2c7be9b0acf1a56b3f584fd1fee028a0266a35c2
Oriol Alàs <oriolalascercos@gmail.com>
2021-05-16 08:20:05 +0200

    merge conflict

--- src/components/WebRTC/CreateMeet.js
+++ src/components/WebRTC/CreateMeet.js
@@ -49,36 +49,10 @@ export default function CreateMeet() {
     });
   }
 
-<<<<<<< HEAD
   return (
     <div className="container">
       <div className="container pt-3" align="center">
         <h1>Create a SpeedDating</h1>
-=======
-    return (
-        <div className="container">
-          <div className="row d-inline">
-          <div className = "row" align = "center" style={{padding:"10px"}}>
-            <div className = "col-xs-12 col-sm-12 col-md-6 col-lg-6" align = "center" >
-              <video ref={localVideo} width="100%" height="100%" autoPlay playsInline controls={false} />
-            </div>
-            <div className = "col-xs-12 col-sm-12 col-md-6 col-lg-6" align = "center">
-              <video width="100%" height="100%" autoPlay playsInline controls={false} ref={remoteVideo} />
-            </div>
-            </div>
-            <div className = "col-xs-12 col-sm-12 col-md-12 col-lg-12" align = "center" style={{padding:"10px"}}>
-              <div class="form-floating">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px"}}></textarea>
-                <label for="floatingTextarea2">Comments</label>
-              </div>
-              <div className = "col-xs-12 col-sm-12 col-md-12 col-lg-12" style={{"padding-top" : "15px"}}>
-              <Button variant="contained" onClick={() => createPeer()} color="primary">
-                Create Meet
-              </Button>
-              </div>
-          </div>
-          </div>
->>>>>>> eda0b6700a6662cb48c64be3cdffd0ac9811af4d
       </div>
       <form align="left" className="pt-3">
         <div className="gradientmeet arrodonit pb-3 pl-2 pr-2">
@@ -148,11 +122,27 @@ export default function CreateMeet() {
         </div>
       </form>
 
-      {opened && (<Button type="submit" variant="contained" onClick={() => createPeer()} color="primary">
-        Create Meet
-            </Button>)}
-      <video ref={localVideo} width="320" height="240" autoPlay playsInline controls={false} />
-      <video width="320" height="240" autoPlay playsInline controls={false} ref={remoteVideo} />
+    <div className="row d-inline">
+          <div className = "row" align = "center" style={{padding:"10px"}}>
+            <div className = "col-xs-12 col-sm-12 col-md-6 col-lg-6" align = "center" >
+              <video ref={localVideo} width="100%" height="100%" autoPlay playsInline controls={false} />
+            </div>
+            <div className = "col-xs-12 col-sm-12 col-md-6 col-lg-6" align = "center">
+              <video width="100%" height="100%" autoPlay playsInline controls={false} ref={remoteVideo} />
+            </div>
+            </div>
+            <div className = "col-xs-12 col-sm-12 col-md-12 col-lg-12" align = "center" style={{padding:"10px"}}>
+              <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px"}}></textarea>
+                <label for="floatingTextarea2">Comments</label>
+              </div>
+              <div className = "col-xs-12 col-sm-12 col-md-12 col-lg-12" style={{"padding-top" : "15px"}}>
+              {opened && (<Button variant="contained" onClick={() => createPeer()} color="primary">
+                Create Meet
+              </Button>)}
+              </div>
+          </div>
+          </div>
     </div>
   );
 }
